112b975406ccaa37c61e946d36acd40d5c05a940
peter-shoes <bcm73@drexel.edu>
2019-04-23 11:35:09 -0400

    stable and functional seasons

--- seasons.py
+++ seasons.py
@@ -63,6 +63,31 @@ def season_dev_winter(m1, m2, m3):
     return(season_all)
 
 
+def deviation(base, match):
+    season_deviation = []
+    for i in match:
+        quant = float('%0.1f'%(i-base))
+        season_deviation.append(quant)
+    return(season_deviation)
+
+
+def deviation_graph(deviation_list, season_name):
+    year_list = []
+    deviation_dict = {}
+    for year in range(1981, 2019):
+        year_list.append(year)
+    for i in range(len(deviation_list)):
+        deviation_dict.update({year_list[i]:deviation_list[i]})
+    print('Average Temperature Deviation by year for', season_name)
+    pp.pprint(deviation_dict)
+    print('')
+    deviation = pd.DataFrame(data=deviation_dict,index=deviation_dict.keys(), dtype=None, columns=None)
+    # plot temp_deviation column graph, display
+    sns.catplot(data=deviation, kind='bar')
+    plt.show()
+
+
+
 def main():
     # Season average 1950-1980
     winter_base_avg = base(sec3, 'Dec', 'Jan', 'Feb')
@@ -76,12 +101,23 @@ def main():
     summer_avg = base(sec4, 'June', 'July', 'Aug')
     fall_avg = base(sec4, 'Sept', 'Oct', 'Nov')
 
-    #Temperature deviation by season by year
+    #Temperature average by season by year
     winter_avg_yby = season_dev_winter('Dec', 'Jan', 'Feb')
     spring_avg_yby = season_dev_nonwinter('March', 'April', 'May')
     summer_avg_yby = season_dev_nonwinter('June', 'July', 'Aug')
     fall_avg_yby =season_dev_nonwinter('Sept', 'Oct', 'Nov')
 
+    # Temperature deviation by season by year
+    winter_deviation = deviation(winter_base_avg, winter_avg_yby)
+    spring_deviation = deviation(spring_base_avg, spring_avg_yby)
+    summer_deviation = deviation(summer_base_avg, summer_avg_yby)
+    fall_deviation = deviation(fall_base_avg, fall_avg_yby)
+
+    # test for graph
+    deviation_graph(winter_deviation, 'Winter')
+    deviation_graph(spring_deviation, 'Spring')
+    deviation_graph(summer_deviation, 'Summer')
+    deviation_graph(fall_deviation, 'Fall')
 
 
 if __name__=='__main__':
